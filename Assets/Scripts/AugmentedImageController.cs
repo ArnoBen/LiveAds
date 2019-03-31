3bd45b374231afe28da91a10e6caa539e62484db
ArnoBen <arno.benizri@gmail.com>
2019-04-01 01:07:44 +0200

    commit with 5 working ads

---
+++ Assets/Scripts/AugmentedImageController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GoogleARCore;
+using System;
+
+public class AugmentedImageController : MonoBehaviour
+{
+    [SerializeField] AugmentedImageVisualizer augmentedImageVisualizer;
+    private Dictionary<int, AugmentedImageVisualizer> visualizers =
+        new Dictionary<int, AugmentedImageVisualizer>();
+
+    private List<AugmentedImage> images = new List<AugmentedImage>();
+
+    void Update()
+    {
+        if (Session.Status != SessionStatus.Tracking)
+            return;
+        Session.GetTrackables<AugmentedImage>(images, TrackableQueryFilter.Updated);
+        VisualizeTrackable();
+    }
+
+    private void VisualizeTrackable()
+    {
+        foreach (var image in images)
+        {
+            AugmentedImageVisualizer visualizer = null;
+            visualizers.TryGetValue(image.DatabaseIndex, out visualizer);
+
+            if (image.TrackingState == TrackingState.Tracking && visualizer == null)
+            {
+                Anchor anchor = image.CreateAnchor(image.CenterPose);
+                visualizer = Instantiate(augmentedImageVisualizer, anchor.transform);
+                visualizer.Image = image;
+                visualizers.Add(image.DatabaseIndex, visualizer);
+            }
+            else if (image.TrackingState == TrackingState.Stopped && visualizer != null)
+            {
+                visualizers.Remove(image.DatabaseIndex);
+                Destroy(visualizer.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/AugmentedImageVisualizer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using GoogleARCore;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class AugmentedImageVisualizer : MonoBehaviour
+{
+    [SerializeField] VideoClip[] videoClips;
+    public AugmentedImage Image;
+    VideoPlayer videoPlayer;
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.loopPointReached += OnStop;
+    }
+
+    private void OnStop(VideoPlayer source)
+    {
+        gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Image == null || Image.TrackingState != TrackingState.Tracking)
+            return;
+        if (!videoPlayer.isPlaying)
+        {
+            videoPlayer.clip = videoClips[Image.DatabaseIndex];
+            videoPlayer.Play();
+        }
+        transform.localScale = new Vector3(Image.ExtentX, Image.ExtentZ, 1);
+    }
+}
